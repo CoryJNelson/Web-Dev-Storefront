8034fb6a50fac4139863c34c2df53f35b4ab6541
Cory Nelson <coryjnelsonn@gmail.com>
2024-12-08 03:04:19 -0800

    Updated Navbar.jsx using styled-components.

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import { AuthContext } from '../components/AuthContext'
+import styled from 'styled-components'
 
 const Navbar = () => {
   const { user, logout } = useContext(AuthContext);
@@ -11,42 +12,75 @@ const Navbar = () => {
     navigate('/');
   };
 
+  // styled components
+  const Container = styled.div`
+    height: 60px;
+    background-color: black;
+  `;
+
+  const Wrapper = styled.div`
+    height: 60px;
+    padding: 0px 20px;
+    color: white;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  `;
+
+  const Left = styled.div`
+    flex: 1;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  `;
+  const Logo = styled.h1`
+    font-weight: bold;
+    cursor: pointer;
+    margin-left: 25px;
+  `
+  const Center = styled.div`
+    flex: 1;
+  `;
+  const Right = styled.div`
+    flex: 1;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  `;
+
+  const NavItem = styled.div`
+    font-size: 18px;
+    cursor: pointer;
+    margin-left: 25px;
+  `;
+
   return (
-    <nav style={styles.navbar}>
-        {/* <h1 style={styles.logo}>Jolt</h1> */}
-        <div style={styles.logo}>
-          <Link to="/" style={styles.link}>Jolt</Link>
-        </div>
-
-        <ul style={styles.navLinks}>
-            <li><Link to="/products" style={styles.link}>Products</Link></li>
-            <li><Link to="/about" style={styles.link}>About</Link></li>
-        </ul>
-
-        <ul style={styles.right}>
+    <Container>
+      <Wrapper>
+        <Left>
+          <Link to="/" style={styles.link}><Logo>JOLT</Logo></Link>
+          <Link to="/products" style={styles.link}><NavItem>Products</NavItem></Link>
+          <Link to="/about" style={styles.link}><NavItem>About</NavItem></Link>
+        </Left>
+        <Center></Center>
+        <Right>
           {user ? (
             <>
-              <Link to={`/users/${user._id}`} style={styles.link}>Account</Link>
+              <Link to={`/users/${user._id}`} style={styles.link}><NavItem>Account</NavItem></Link>
               <button onClick={handleLogout} style={styles.logoutButton}>Sign Out</button>
             </>
           ) : (
-            <li><Link to="/login" style={styles.link}>Login</Link></li>
+            <Link to="/login" style={styles.link}><NavItem>Login</NavItem></Link>
           )}
-          <li><Link to="/cart" style={styles.link}>Cart</Link></li>
-        </ul>
-    </nav>
+          <Link to="/cart" style={styles.link}><NavItem>Cart</NavItem></Link>
+        </Right>
+      </Wrapper>
+    </Container>
   )
 }
 
 const styles = {
-    navbar: { 
-      display: 'flex', justifyContent: 'space-between', alignItems: 'center',
-      padding: '1rem', background: '#222', color: '#fff' 
-    },
-    logo: { fontSize: '2rem', fontWeight: 'bold' },
-    navLinks: { fontSize: '1.5rem', listStyle: 'none', display: 'flex', gap: '2rem' },
     link: { color: '#fff', textDecoration: 'none' },
-    right: { fontSize: '1.5rem', display: 'flex', gap: '2rem'},
     logoutButton: { background: '#222', color: '#fff', cursor: 'pointer', padding: '0 1rem' }
 };
 
